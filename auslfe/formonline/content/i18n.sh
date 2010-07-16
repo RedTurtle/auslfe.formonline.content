9634fd0f064efcad3ac3c198a78a9dfe2166b400
federica.delia <federica.delia@c6e3998e-5644-4537-8003-e402e8770f0f>
2010-07-16 10:24:48 +0000

    notification email

--- auslfe/formonline/content/i18n.sh
+++ auslfe/formonline/content/i18n.sh
@@ -3,7 +3,7 @@
 DOMAIN='auslfe.formonline.content'
 
 i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --create ${DOMAIN} .
-
+i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --merge locales/${DOMAIN}-manual.pot --create ${DOMAIN} .
 i18ndude sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
 
 # Compile po files
