41dee896c3a29e7cb8ec9335dc4b0a20c79b1265
luca.fabbri <luca.fabbri@c6e3998e-5644-4537-8003-e402e8770f0f>
2012-01-10 15:50:48 +0000

    small Plone 4 compatibility

--- auslfe/formonline/content/setuphandlers.py
+++ auslfe/formonline/content/setuphandlers.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from Products.CMFCore.utils import getToolByName
-from Products.CMFEditions.setuphandlers import DEFAULT_POLICIES
+try:
+    from Products.CMFEditions.setuphandlers import DEFAULT_POLICIES
+    PLONE3 = True
+except ImportError:
+    PLONE3 = False
 
 TYPES_TO_VERSION = ('FormOnline',)
 
@@ -17,19 +21,20 @@ def setVersionedTypes(context):
     
     '''
 
-    if context.readDataFile('auslfe.formonline.content_types.txt') is None:
-        return
-
-    portal=context.getSite()
-    portal_repository = getToolByName(portal, 'portal_repository')
-    versionable_types = list(portal_repository.getVersionableContentTypes())
-    for type_id in TYPES_TO_VERSION:
-        if type_id not in versionable_types:
-            # use append() to make sure we don't overwrite any
-            # content-types which may already be under version control
-            versionable_types.append(type_id)
-            # Add default versioning policies to the versioned type
-            for policy_id in DEFAULT_POLICIES:
-                portal_repository.addPolicyForContentType(type_id, policy_id)
-     
-    portal_repository.setVersionableContentTypes(versionable_types)
+    if PLONE3:
+        if context.readDataFile('auslfe.formonline.content_types.txt') is None:
+            return
+    
+        portal=context.getSite()
+        portal_repository = getToolByName(portal, 'portal_repository')
+        versionable_types = list(portal_repository.getVersionableContentTypes())
+        for type_id in TYPES_TO_VERSION:
+            if type_id not in versionable_types:
+                # use append() to make sure we don't overwrite any
+                # content-types which may already be under version control
+                versionable_types.append(type_id)
+                # Add default versioning policies to the versioned type
+                for policy_id in DEFAULT_POLICIES:
+                    portal_repository.addPolicyForContentType(type_id, policy_id)
+         
+        portal_repository.setVersionableContentTypes(versionable_types)
